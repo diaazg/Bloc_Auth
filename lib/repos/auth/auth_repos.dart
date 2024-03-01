ab2dfc63dbe7266a41cb4c41fb2c4d4aa73b55b2
diaazg <mohamedzellagui585@gmail.com>
2024-03-01 16:40:13 +0100

    create home repo methods

--- lib/repos/auth/auth_repos.dart
+++ lib/repos/auth/auth_repos.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:prisma_app_note/error/failures.dart';
+
 abstract class AuthRepo {
-  login();
-  register();
+  Future<Either<Failure, String>> login(String email, String password);
+  Future<Either<Failure, String>> register(String email, String password);
 }
